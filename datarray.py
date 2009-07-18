99e50b69ed0cb6d52c27f07ad65094ae293ed943
Fernando Perez <Fernando.Perez@berkeley.edu>
2009-07-17 19:10:17 -0700

    First correct support of indexing.
    Now some tests pass with proper scalar indexing.

--- datarray.py
+++ datarray.py
@@ -9,16 +9,24 @@ Questions
 - Transposition
 - Swapaxes
 - Rollaxes
+- Iteration
 - Wrapping functions with 'axis=' kw.
 
 """
 
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
 import copy
 
+import nose.tools as nt
 import numpy as np
+import numpy.testing as npt
 
-
-### classes
+#-----------------------------------------------------------------------------
+# Classes and functions
+#-----------------------------------------------------------------------------
 
 class Axis(object):
     """Object to access a given axis of an array.
@@ -34,11 +42,20 @@ class Axis(object):
         return self.arr.shape[self.index]
     
     def __getitem__(self, key):
+        if isinstance(key, slice):
+            # Dimensionality of output is dimensionality of input
+            pass
+        else:
+            # Scalar indexing, output has 1-d less
+            if self.arr.ndim==1:
+                return self.arr[key]
         # Make a slice
         nullslice = slice(None)
         fullslice = [nullslice] * self.arr.ndim
         fullslice[self.index] = key
+        print 'FS:',fullslice
         return self.arr[fullslice]
+
         
 
 class UnnamedAxis(Axis):
@@ -56,15 +73,23 @@ def copy_names(src,dest):
         setattr(dest, aname, newaxis)
 
 
+def names2namedict(names):
+    """Make a name map out of any name input.
+    """
+    raise NotImplementedError() 
+
+
 class DataArray(np.ndarray):
+
+    # XXX- we need to figure out where in the numpy C code .T is defined!
     @property
     def T(self):
         return self.transpose()
+
         
     def __new__(cls, data, names=None, dtype=None, copy=False):
         # Ensure the output is an array of the proper type
-        arr = np.array(data, dtype=dtype, copy=copy)
-        arr = arr.view(cls)
+        arr = np.array(data, dtype=dtype, copy=copy).view(cls)
 
         # Sanity check: if names are given, it must be a sequence no  longer
         # than the array shape
@@ -77,27 +102,53 @@ class DataArray(np.ndarray):
             for i,name in enumerate(names):
                 setattr(arr,'a_%s' % name,Axis(name, i, arr))
             arr.names = names
+
         # Or if the input had named axes, copy them over
         elif hasattr(data,'names'):
             copy_names(data, arr)
             
         return arr
 
+
     def __array_finalize__(self, obj):
+        """Called by ndarray on subobject (like views/slices) creation.
+
+        self: new object just made.
+        obj: old object from which self was made.
+        """
+        # Ref: see http://docs.scipy.org/doc/numpy/user/basics.subclassing.html
+        
         # provide info for what's happening
-        print "finalize:\t%s\n\t\t%s" % (self.__class__, obj.__class__) # dbg
+        #print "finalize:\t%s\n\t\t%s" % (self.__class__, obj.__class__) # dbg
         # provide more info
         if hasattr(obj,'names'):
             copy_names(obj,self)
+
 
     def transpose(self, *axes):
         print 'hi'
 
 
-### main
-dt = np.dtype([('f1',float), ('f2',float)])
+#-----------------------------------------------------------------------------
+# Tests
+#-----------------------------------------------------------------------------
+if 1:
+    adata = [2,3]
+    a = DataArray(adata, 'x', int)
+    print a.a_x[0]
+    
+def test1():
 
-a = DataArray([2,3], 'x', int)
-b = DataArray([[1,2],[3,4]], 'xy')
+    adata = [2,3]
+    a = DataArray(adata, 'x', int)
 
-print b.a_y[1].names
+    yield (nt.assert_true,isinstance(a.a_x[0],int))
+    
+    yield (nt.assert_equals, a.a_x.index, 0)
+    for i,val in enumerate(a.a_x):
+        yield (nt.assert_equals,val,adata[i])
+        yield (nt.assert_true,isinstance(val,int))
+
+    b = DataArray([[1,2],[3,4]], 'xy')
+    yield (nt.assert_equals, b.names, ['x','y'])
+
