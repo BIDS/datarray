989f4551ebe8138fa3926ba6d7b9970ed43d4908
Joshua Holbrook <josh.holbrook@gmail.com>
2010-07-07 14:25:58 -0800

    forgot to commit changed conf.py which tells sphinx to use sphinxtogithub

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -17,12 +17,14 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.append(os.path.abspath('.'))
+sys.path.append(os.path.abspath('./extensions'))
+#^^Added for grabbing github action
 
 # -- General configuration -----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.pngmath']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.pngmath', 'sphinxtogithub']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
