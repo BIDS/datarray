48cb2c27e2a518ca3b7aca6d6ebef63e4aef0b21
Fernando Perez <Fernando.Perez@berkeley.edu>
2010-07-28 19:19:30 -0700

    Add proper README and long description to setup.py for PyPI.

--- README.txt
+++ README.txt
@@ -1,5 +1,35 @@
-============================
-Numpy arrays with named axes
-============================
+========================================
+ Datarray: Numpy arrays with named axes
+========================================
 
-Note: this is for now experimental code.
+Datarray provides a subclass of Numpy ndarrays that support:
+
+- individual dimensions (axes) being labeled with meaningful descriptions
+- labeled 'ticks' along each axis
+- indexing and slicing by named axis
+- indexing on any axis with the tick labels instead of only integers
+- reduction operations (like .sum, .mean, etc) support named axis arguments
+  instead of only integer indices.
+
+  
+Code
+====
+
+You can find our sources and single-click downloads:
+
+* `Main repository`_ on Github.
+* Download as a tar/zip file the `current trunk`_.
+* Downloads of all `available releases`_.
+
+.. _main repository: http://github.com/fperez/datarray
+.. _current trunk: http://github.com/fperez/datarray/archives/master
+.. _available releases: http://github.com/fperez/datarray/downloads
+
+
+Note
+====
+
+this code is currently experimental!  It is meant to be a place for the
+community to understand and develop the right semantics and have a prototype
+implementation that will ultimately (hopefully) be folded back into Numpy
+itself.
--- datarray/version.py
+++ datarray/version.py
@@ -16,11 +16,46 @@ CLASSIFIERS = ["Development Status :: 3 - Alpha",
                "Topic :: Scientific/Engineering"]
 
 description = "NumPy arrays with named axes and named indices."
-long_description = """
-Who's datarray?
-===============
 
-TODO.
+# Note: this long_description is actually a copy/paste from the top-level
+# README.txt, so that it shows up nicely on PyPI.  So please remember to edit
+# it only in one place and sync it correctly.
+long_description = """
+========================================
+ Datarray: Numpy arrays with named axes
+========================================
+
+Datarray provides a subclass of Numpy ndarrays that support:
+
+- individual dimensions (axes) being labeled with meaningful descriptions
+- labeled 'ticks' along each axis
+- indexing and slicing by named axis
+- indexing on any axis with the tick labels instead of only integers
+- reduction operations (like .sum, .mean, etc) support named axis arguments
+  instead of only integer indices.
+
+  
+Code
+====
+
+You can find our sources and single-click downloads:
+
+* `Main repository`_ on Github.
+* Download as a tar/zip file the `current trunk`_.
+* Downloads of all `available releases`_.
+
+.. _main repository: http://github.com/fperez/datarray
+.. _current trunk: http://github.com/fperez/datarray/archives/master
+.. _available releases: http://github.com/fperez/datarray/downloads
+
+
+Note
+====
+
+this code is currently experimental!  It is meant to be a place for the
+community to understand and develop the right semantics and have a prototype
+implementation that will ultimately (hopefully) be folded back into Numpy
+itself.
 """
 
 
