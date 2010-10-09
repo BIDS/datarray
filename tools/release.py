ea345b4aa62c97e618e160963e01eee8c81b0112
Fernando Perez <Fernando.Perez@berkeley.edu>
2010-10-08 18:59:20 -0700

    Add simple release script.

---
+++ tools/release.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""Simple release script for datarray.
+
+Ensure that you've built the docs and pushed those first (after veryfing them
+manually).
+"""
+from __future__ import print_function
+
+import os
+from subprocess import call as sh
+
+cwd = os.getcwd()
+if not os.path.isfile('setup.py'):
+    os.chdir('..')
+    if not os.path.isfile('setup.py'):
+        print("This script must be run from top-level datarray or tools dir.")
+        sys.exit(1)
+
+
+sh('./setup.py register')
+sh('./setup.py sdist --formats=gztar,zip upload')
