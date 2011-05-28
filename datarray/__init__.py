69724c05a38000c3d5fb141f4c0d79991fa16cea
Matthew Brett <matthew.brett@gmail.com>
2011-05-27 17:36:36 -0700

    BF+DOC - update, clarify numpy dependency
    Numpy dependency of >= 1.4.0 seems to be due to need for
    __array_prepare__ method.

--- datarray/__init__.py
+++ datarray/__init__.py
@@ -8,8 +8,9 @@ import distutils.version as v
 
 # Third-party
 import numpy as np
+# datarray uses the __array_prepare__ method introduced in numpy 1.4.0
 if v.LooseVersion(np.__version__) < v.LooseVersion('1.4'):
-    raise ImportError('Numpy version > 1.4 is required to use datarray')
+    raise ImportError('Numpy version >= 1.4 is required to use datarray')
 
 # Our own
 try:
